1a26d4aa88f93aadcfd3b896a67c52ded1c9434d
KaSt <k@ka.st>
2013-02-15 09:29:38 +0100

    Initial Commit with CPU Governors, thanks to @DooMLoRD

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 6 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 PREEMPT Thu Feb 14 22:30:04 CET 2013"
+#define LINUX_COMPILE_TIME "22:30:04"
+#define LINUX_COMPILE_BY "ka"
+#define LINUX_COMPILE_HOST "ti.ka.st"
+#define LINUX_COMPILE_DOMAIN "ka.st"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
