a69efe290e5b8934f157c4a7fb6cd0f90873d627
KaSt <k@ka.st>
2013-02-18 15:31:32 +0100

    Stable Initial version

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 9 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Thu Feb 14 22:30:04 CET 2013"
-#define LINUX_COMPILE_TIME "22:30:04"
+#define UTS_VERSION "#9 PREEMPT Fri Feb 15 12:14:34 CET 2013"
+#define LINUX_COMPILE_TIME "12:14:34"
 #define LINUX_COMPILE_BY "ka"
 #define LINUX_COMPILE_HOST "ti.ka.st"
 #define LINUX_COMPILE_DOMAIN "ka.st"
