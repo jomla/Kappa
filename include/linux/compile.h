ee99b42a4a2598230d377ecd3e684643a1e1c7f1
KaSt <k@ka.st>
2013-02-18 17:02:26 +0100

    Added SLQB
    Signed-off-by: KaSt <k@ka.st>

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Mon Mar 11 23:23:15 CET 2013"
-#define LINUX_COMPILE_TIME "23:23:15"
+#define UTS_VERSION "#1 PREEMPT Tue Mar 12 00:59:58 CET 2013"
+#define LINUX_COMPILE_TIME "00:59:58"
 #define LINUX_COMPILE_BY "ka"
 #define LINUX_COMPILE_HOST "ti.ka.st"
 #define LINUX_COMPILE_DOMAIN "ka.st"
