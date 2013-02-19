cc31b585556c089294a19f4ee905b299203fd495
KaSt <k@ka.st>
2013-02-19 11:53:25 +0100

    Fixes SD card issue

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 PREEMPT Mon Feb 18 16:01:12 CET 2013"
-#define LINUX_COMPILE_TIME "16:01:12"
+#define UTS_VERSION "#11 PREEMPT Tue Feb 19 10:40:19 CET 2013"
+#define LINUX_COMPILE_TIME "10:40:19"
 #define LINUX_COMPILE_BY "ka"
 #define LINUX_COMPILE_HOST "ti.ka.st"
 #define LINUX_COMPILE_DOMAIN "ka.st"
