dba755ff0c90007bdf877f722cb67ae279becd50
KaSt <k@ka.st>
2013-03-02 09:57:47 +0100

    Boot Boost (1.6GHZ) : Reduced max OC to 1.7GHZ

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 PREEMPT Wed Feb 27 11:36:01 CET 2013"
-#define LINUX_COMPILE_TIME "11:36:01"
+#define UTS_VERSION "#3 PREEMPT Fri Mar 1 22:25:17 CET 2013"
+#define LINUX_COMPILE_TIME "22:25:17"
 #define LINUX_COMPILE_BY "Ka"
 #define LINUX_COMPILE_HOST "Kappa"
 #define LINUX_COMPILE_DOMAIN "Kappa"
