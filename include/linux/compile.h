da6d97149f1242295537279a349814492cffafec
KaSt <k@ka.st>
2013-03-11 23:45:32 +0100

    Linaro 4.7.3 changes

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Thu Feb 14 22:30:04 CET 2013"
-#define LINUX_COMPILE_TIME "22:30:04"
+#define UTS_VERSION "#1 PREEMPT Mon Mar 11 23:23:15 CET 2013"
+#define LINUX_COMPILE_TIME "23:23:15"
 #define LINUX_COMPILE_BY "ka"
 #define LINUX_COMPILE_HOST "ti.ka.st"
 #define LINUX_COMPILE_DOMAIN "ka.st"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
+#define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
