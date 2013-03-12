db7e001bc76c53007c7a1e740321c3eddfa0c4c8
KaSt <k@ka.st>
2013-03-12 02:01:01 +0100

    [PATCH] Added Intellidemand CPU Governor Governors updated to new  freqs (thanks @droidphile), plus Lulzctive optimisations by  droidphile Google Snappy Compress&Decompress for ZRam

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Tue Mar 12 01:49:34 CET 2013"
-#define LINUX_COMPILE_TIME "01:49:34"
-#define LINUX_COMPILE_BY "ka"
-#define LINUX_COMPILE_HOST "ti.ka.st"
-#define LINUX_COMPILE_DOMAIN "ka.st"
+#define UTS_VERSION "#5 PREEMPT Tue Mar 12 01:57:54 CET 2013"
+#define LINUX_COMPILE_TIME "01:57:54"
+#define LINUX_COMPILE_BY "Ka"
+#define LINUX_COMPILE_HOST "Kappa"
+#define LINUX_COMPILE_DOMAIN "Kappa"
 #define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
