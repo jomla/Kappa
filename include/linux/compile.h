3202dccc8c5350ae8a760b7d9162da6ae42e83c6
KaSt <k@ka.st>
2013-03-12 02:50:53 +0100

    [PATCH] Parfum de 2.6.32.60 : Added CPUGOVs -> Darkside, Lionheart,  SmartassH3, Intellidemand2

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 8 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Tue Mar 12 02:17:41 CET 2013"
-#define LINUX_COMPILE_TIME "02:17:41"
+#define UTS_VERSION "#8 PREEMPT Tue Mar 12 02:49:17 CET 2013"
+#define LINUX_COMPILE_TIME "02:49:17"
 #define LINUX_COMPILE_BY "Ka"
 #define LINUX_COMPILE_HOST "Kappa"
 #define LINUX_COMPILE_DOMAIN "Kappa"
