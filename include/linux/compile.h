8b4bf0a146ea710799d440b9d95bd77c4a02b3a6
KaSt <k@ka.st>
2013-03-12 10:45:52 +0100

    Working

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 0 */
+/* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 PREEMPT Tue Mar 12 10:36:05 CET 2013"
-#define LINUX_COMPILE_TIME "10:36:05"
+#define UTS_VERSION "#1 PREEMPT Tue Mar 12 10:45:08 CET 2013"
+#define LINUX_COMPILE_TIME "10:45:08"
 #define LINUX_COMPILE_BY "Ka"
 #define LINUX_COMPILE_HOST "Kappa"
 #define LINUX_COMPILE_DOMAIN "Kappa"
