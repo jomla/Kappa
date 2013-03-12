6d829cbdb4c93e30969efde2aa7c26e80290043c
KaSt <k@ka.st>
2013-03-12 02:21:40 +0100

    Added Memcopy, TinyRCU

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Tue Mar 12 01:57:54 CET 2013"
-#define LINUX_COMPILE_TIME "01:57:54"
+#define UTS_VERSION "#6 PREEMPT Tue Mar 12 02:17:41 CET 2013"
+#define LINUX_COMPILE_TIME "02:17:41"
 #define LINUX_COMPILE_BY "Ka"
 #define LINUX_COMPILE_HOST "Kappa"
 #define LINUX_COMPILE_DOMAIN "Kappa"
